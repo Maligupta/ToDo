078389a660015372b3429df63115e0e5e2a1a495
Malika Gupta <malikagupta@Malikas-MacBook-Pro.local>
2016-07-23 16:16:08 -0700

    first commit

---
+++ Todo/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Todo
+//
+//  Created by Malika Gupta on 7/23/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: NSObject {
+
+}
